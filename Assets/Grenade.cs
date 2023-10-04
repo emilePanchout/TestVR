9f619d8a7dec5db46a0b0a2e42b9b0a8250e2360
DESKTOP-VN8AUHK\Test <1>
2023-10-04 17:33:23 +0200

    particules

--- Assets/Grenade.cs
+++ Assets/Grenade.cs
@@ -7,6 +7,12 @@ public class Grenade : MonoBehaviour
 {
     public XRGrabInteractable grab;
     public bool isLaunched = false;
+
+    public GameObject grenadePrefab;
+    public GameObject particules;
+    public GameObject robot;
+    public FireManager fireManager;
+    public float explosionRadius;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +35,15 @@ public class Grenade : MonoBehaviour
     {
         if (isLaunched && !collision.gameObject.CompareTag("PlayerController"))
         {
+            Instantiate(particules, transform.position, transform.rotation);
+
+            if (explosionRadius > Vector3.Distance(transform.position,robot.transform.position))
+            {
+                fireManager.particules.SetActive(false);
+            }
+
+            //Instantiate(grenadePrefab, transform.parent);
+
             Debug.Log(collision.gameObject.name);
             Destroy(gameObject);
         }
